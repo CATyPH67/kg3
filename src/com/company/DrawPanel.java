59b62ae89def3efc2695f77e997f6d6d6c9570a8
CATyPH67 <031kirill2a@mail.ru>
2021-11-15 09:08:26 +0300

    I do not know what to write here

--- src/com/company/DrawPanel.java
+++ src/com/company/DrawPanel.java
@@ -42,9 +42,7 @@ public class DrawPanel extends JPanel implements MouseListener, MouseMotionListe
 
         for (Polygon polygon : polygons) {
             if (polygon == editingPolygon) {
-                g.setColor(Color.GREEN);
-                drawPolygon(g, sc, polygon);
-                g.setColor(Color.BLACK);
+                drawEditingPolygon(g, sc, polygon);
             } else {
                 drawPolygon(g, sc, polygon);
             }
@@ -68,29 +66,172 @@ public class DrawPanel extends JPanel implements MouseListener, MouseMotionListe
         g.drawLine(p1.getX(), p1.getY(), p2.getX(), p2.getY());
     }
 
+    private static void drawEditingPolygon(Graphics2D g, ScreenConverter sc, Polygon polygon) {
+        Color prevColor = g.getColor();
+        g.setColor(Color.GREEN);
+        ArrayList<RealPoint> points = polygon.getPoints();
+        for (RealPoint point: points) {
+            ScreenPoint sp = sc.r2s(point);
+            g.fillOval(sp.getX() - DELTA / 2, sp.getY() - DELTA / 2, DELTA, DELTA);
+        }
+        drawPolygon(g, sc, polygon);
+        g.setColor(prevColor);
+    }
+
     private static void drawPolygon(Graphics2D g, ScreenConverter sc, Polygon polygon) {
         ArrayList<RealPoint> points = polygon.getPoints();
         if (points.size() > 1) {
-            ScreenPoint p1 = sc.r2s(points.get(0));
-            ScreenPoint p2;
-            for (int i = 1; i < points.size(); i++) {
-                p2 = sc.r2s(points.get(i));
+            int r = polygon.getR();
+            if(r == 0) {
+                ScreenPoint p1 = sc.r2s(points.get(0));
+                ScreenPoint p2;
+                for (int i = 1; i < points.size(); i++) {
+                    p2 = sc.r2s(points.get(i));
+                    g.drawLine(p1.getX(), p1.getY(), p2.getX(), p2.getY());
+                    p1 = p2;
+                }
+                p2 = sc.r2s(points.get(0));
                 g.drawLine(p1.getX(), p1.getY(), p2.getX(), p2.getY());
+            } else {
+                ScreenPoint p1 = sc.r2s(points.get(0));
+                ScreenPoint p2 = sc.r2s(points.get(1));
+                ScreenPoint p3 = sc.r2s(points.get(2));
+                Arch arc = findArc(sc, p1, p2, p3, r, g);
+                int touchPrevX = arc.getTouchX2();
+                int touchPrevY = arc.getTouchY2();
+                int touchPrevX1 = arc.getTouchX1();
+                int touchPrevY1 = arc.getTouchY1();
+                g.drawArc(arc.getX(), arc.getY(), arc.getWidth(), arc.getHeight(), arc.getStartAngle(), arc.getArcAngle());
+                for (int i = 3; i < points.size(); i++) {
+                    p1 = sc.r2s(points.get(i - 2));
+                    p2 = sc.r2s(points.get(i - 1));
+                    p3 = sc.r2s(points.get(i));
+                    arc = findArc(sc, p1, p2, p3, r, g);
+                    g.drawLine(arc.getTouchX1(), arc.getTouchY1(), touchPrevX, touchPrevY);
+                    g.drawArc(arc.getX(), arc.getY(), arc.getWidth(), arc.getHeight(), arc.getStartAngle(), arc.getArcAngle());
+                    touchPrevX = arc.getTouchX2();
+                    touchPrevY = arc.getTouchY2();
+                }
                 p1 = p2;
+                p2 = p3;
+                p3 = sc.r2s(points.get(0));
+                arc = findArc(sc, p1, p2, p3, r, g);
+                g.drawLine(arc.getTouchX1(), arc.getTouchY1(), touchPrevX, touchPrevY);
+                g.drawArc(arc.getX(), arc.getY(), arc.getWidth(), arc.getHeight(), arc.getStartAngle(), arc.getArcAngle());
+                touchPrevX = arc.getTouchX2();
+                touchPrevY = arc.getTouchY2();
+                p1 = p2;
+                p2 = p3;
+                p3 = sc.r2s(points.get(1));
+                arc = findArc(sc, p1, p2, p3, r, g);
+                g.drawLine(arc.getTouchX1(), arc.getTouchY1(), touchPrevX, touchPrevY);
+                g.drawArc(arc.getX(), arc.getY(), arc.getWidth(), arc.getHeight(), arc.getStartAngle(), arc.getArcAngle());
+                touchPrevX = arc.getTouchX2();
+                touchPrevY = arc.getTouchY2();
+                g.drawLine(touchPrevX1, touchPrevY1, touchPrevX, touchPrevY);
             }
-            p2 = sc.r2s(points.get(0));
-            g.drawLine(p1.getX(), p1.getY(), p2.getX(), p2.getY());
         }
+    }
+
+    private static Arch findArc(ScreenConverter sc, ScreenPoint p1, ScreenPoint p2, ScreenPoint p3, int r, Graphics g) {
+        int x, y, startAngle, arcAngle, touchX1, touchY1, touchX2, touchY2;
+        double x0 = p2.getX();
+        double y0 = p2.getY();
+        double x1 = p1.getX();
+        double y1 = p1.getY();
+        double x2 = p2.getX();
+        double y2 = p2.getY();
+        double x3 = p3.getX();
+        double y3 = p3.getY();
+        double a1 = y0 - y1;
+        double b1 = x1 - x0;
+        double a2 = y2 - y3;
+        double b2 = x3 - x2;
+        double cosA = b1 / Math.sqrt(b1 * b1 + a1 * a1);
+        double sinA = Math.sqrt(1 - cosA * cosA);
+        double r1 = r / sinA;
+        cosA = b2 / Math.sqrt(b2 * b2 + a2 * a2);
+        sinA = Math.sqrt(1 - cosA * cosA);
+        double r2 = r / sinA;
+        if (((y2 <= y1) && (y2 <= y3)) || ((y2 >= y1) && (y2 >= y3))) {
+            if (x1 < x3) {
+                x0 += r1;
+                x1 += r1;
+                x2 -= r2;
+                x3 -= r2;
+            } else {
+                x0 -= r1;
+                x1 -= r1;
+                x2 += r2;
+                x3 += r2;
+            }
+        } else if ((x2 <= x1) && (x2 <= x3)) {
+            x0 += r1;
+            x1 += r1;
+            x2 += r2;
+            x3 += r2;
+        } else if ((x2 >= x1) && (x2 >= x3)) {
+            x0 -= r1;
+            x1 -= r1;
+            x2 -= r2;
+            x3 -= r2;
+        }
+        a1 = y0 - y1;
+        b1 = x1 - x0;
+        double c1 = x0 * y1 - x1 * y0;
+        a2 = y2 - y3;
+        b2 = x3 - x2;
+        double c2 = x2 * y3 - x3 * y2;
+        double det = a1 * b2 - a2 * b1;
+        x = (int)((b1 * c2 - b2 * c1) / det);
+        y = (int)((a2 * c1 - a1 * c2) / det);
+
+        x2 = p2.getX();
+        y2 = p2.getY();
+
+        double l = findDistance(new ScreenPoint(x, y), p2);
+        double d = Math.sqrt(l * l - r * r);
+        double cs = (x - x2) / l;
+        double u1;
+        if (y < y2) {
+            u1 = Math.acos(cs);
+        } else {
+            u1 = 2 * Math.PI - Math.acos(cs);
+        }
+        cs = d / l;
+        double u2 = Math.acos(cs);
+        double u = u1 - u2;
+        touchX1 = (int)(x2 + d * Math.cos(u));
+        touchY1 = (int)(y2 - d * Math.sin(u));
+        u = u1 + u2;
+        touchX2 = (int)(x2 + d * Math.cos(u));
+        touchY2 = (int)(y2 - d * Math.sin(u));
+
+        startAngle = (int)Math.toDegrees(Math.atan2(y - touchY1, touchX1 - x));
+        arcAngle = (int)Math.toDegrees(Math.atan2(y - touchY2, touchX2 - x));
+
+        return new Arch(x - r, y - r, r * 2, r * 2, startAngle, (int)angleDiff(startAngle, arcAngle),
+                touchX1, touchY1, touchX2, touchY2);
+    }
+
+    static double angleDiff(double a, double b) {
+        double d = b - a;
+        while (d >= 180f) { d -= 360f; }
+        while (d < -180f) { d += 360f; }
+        return d;
     }
 
     private static final int DELTA = 10;
     private static boolean isPointNearby(ScreenPoint p1, ScreenPoint p2, int delta) {
-        double distance = Math.sqrt(Math.pow(p2.getX() - p1.getX(), 2) + Math.pow(p2.getY() - p1.getY(), 2));
+        double distance = findDistance(p1, p2);
         if (delta > distance) {
             return true;
         } else {
             return false;
         }
+    }
+    private static double findDistance(ScreenPoint p1, ScreenPoint p2) {
+        return Math.sqrt(Math.pow(p2.getX() - p1.getX(), 2) + Math.pow(p2.getY() - p1.getY(), 2));
     }
 
     private static boolean isLineNearby(ScreenConverter sc, ScreenPoint p0, Line line, int delta) {
@@ -161,7 +302,11 @@ public class DrawPanel extends JPanel implements MouseListener, MouseMotionListe
     @Override
     public void mousePressed(MouseEvent e) {
         ScreenPoint currentPoint = new ScreenPoint(e.getX(), e.getY());
-        if (SwingUtilities.isRightMouseButton(e)) {
+        if (SwingUtilities.isMiddleMouseButton(e)) {
+            if (editingPolygon != null) {
+                firstPoint = currentPoint;
+            }
+        } else if (SwingUtilities.isRightMouseButton(e)) {
             firstPoint = currentPoint;
         } else if (SwingUtilities.isLeftMouseButton(e)) {
             if (newPolygon != null) {
@@ -185,7 +330,9 @@ public class DrawPanel extends JPanel implements MouseListener, MouseMotionListe
     @Override
     public void mouseReleased(MouseEvent e) {
         ScreenPoint currentPoint = new ScreenPoint(e.getX(), e.getY());
-        if (SwingUtilities.isRightMouseButton(e)) {
+        if (SwingUtilities.isMiddleMouseButton(e)) {
+            firstPoint = null;
+        } else if (SwingUtilities.isRightMouseButton(e)) {
             firstPoint = null;
         } else if (SwingUtilities.isLeftMouseButton(e)) {
             if (newPolygon != null) {
@@ -219,7 +366,16 @@ public class DrawPanel extends JPanel implements MouseListener, MouseMotionListe
     @Override
     public void mouseDragged(MouseEvent e) {
         ScreenPoint currentPoint = new ScreenPoint(e.getX(), e.getY());
-        if (SwingUtilities.isRightMouseButton(e)) {
+        if (SwingUtilities.isMiddleMouseButton(e)) {
+            if (editingPolygon != null) {
+                int r = (int)findDistance(firstPoint, currentPoint) - DELTA;
+                if (r > 0) {
+                    editingPolygon.setR(r);
+                } else {
+                    editingPolygon.setR(0);
+                }
+            }
+        } else if (SwingUtilities.isRightMouseButton(e)) {
             RealPoint p1 = sc.s2r(currentPoint);
             RealPoint p2 = sc.s2r(firstPoint);
             RealPoint delta = p2.minus(p1);
